ad1ecc6243ecc0d35c86df8eba6ab7b47d49dee1
Andi N <a.nenning91@gmail.com>
2016-01-26 19:02:00 +0100

    Djikstra Call + Popup + Visualization

--- maps_cc.R
+++ maps_cc.R
@@ -1,6 +1,14 @@
 require(leaflet)
 require(RMySQL)
 require(geosphere)
+require(RODBC)
+
+conn <- odbcConnect(dsn = "cc_test")
+start <- 1
+end <- 416
+sql_statement = paste("EXEC usp_getShortestPath ",start,", ",end,";",sep = "")
+queryResult <- sqlQuery(conn, sql_statement,errors = FALSE)
+odbcClose(conn)
 
 
 mydb = dbConnect(MySQL(), user='root', password='root', dbname='graphenesqlbase', host='localhost')
@@ -23,12 +31,13 @@ edges$tocenter_lat <- as.numeric(edges$tocenter_lat)
 a <- data.frame(edges$fromcenter_long,edges$fromcenter_lat)
 b <- data.frame(edges$tocenter_long,edges$tocenter_lat)
 
-gcIntermediate(a,
-               b,
-               50,
-               breakAtDateLine = FALSE,
-               addStartEnd = TRUE,
-               sp = TRUE) %>% 
+# gcIntermediate(a,
+#                b,
+#                50,
+#                breakAtDateLine = FALSE,
+#                addStartEnd = TRUE,
+#                sp = TRUE) %>% 
 
 
-leaflet() %>% setView(lng = 40,lat = 50, zoom = 3) %>% addTiles() %>% addCircleMarkers(lng = nodes$center_long, lat = nodes$center_lat,radius = 0.5,color = "red", fillOpacity = 0.5) %>% addPolylines(opacity = 0.2,color = "blue")
+
+leaflet() %>% setView(lng = 40,lat = 50, zoom = 3) %>% addTiles() %>% addCircleMarkers(lng = nodes$center_long, lat = nodes$center_lat,radius = 0.5,color = "red", fillOpacity = 0.5, popup = paste("Longitude: ",nodes$center_long,"<br />Latitude: ",nodes$center_lat,sep ="")) %>%  addPolylines(lat = queryResult$center_lat,lng = queryResult$center_long)
